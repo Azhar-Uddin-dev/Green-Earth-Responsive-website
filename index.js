a1080127a4fbcdfbf841856323693c661ac8be96
Azhar Uddin <azhar734499@gmail.com>
2025-09-08 10:50:04 +0600

    added an plantcategory

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const levelcontainer = document.getElementById("level-container");
 
+//load categories
+const plantcontainer = document.getElementById("plants-container");
+
 const loadcategory = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
         .then(res => res.json())
@@ -11,6 +14,25 @@ const loadcategory = () => {
             console.log(error);
         });
 };
+//show categories
+const showPlantsCategories=(plants) => {
+   plantcontainer.innerHTML =``; 
+plants.forEach(plant => {
+    plantcontainer.innerHTML += `
+             <div class="p-4 w-50 bg-white rounded-lg shadow-md space-y-2 ">
+                  <img src="${plant.image}" alt="${plant.name}">
+                  <h1 class="text-lg font-semibold">${plant.category}</h1>
+                  <p class="text-sm">${plant.description ? plant.description : 'No description available.'}</p>
+                  <div class="flex justify-between items-center">
+                    <a class="btn bg-green-100 rounded-full mt-2">Fruit Tree</a>
+                    <p>৳${plant.price}</p>
+                  </div>
+                  <button class="btn bg-green-500 rounded-full mt-2">Add to Cart</button>
+                 </div>
+    `;
+});
+
+}
 
 const showCategories = (categories) => {
     
@@ -19,9 +41,12 @@ const showCategories = (categories) => {
     
     categories.forEach(cat => {
         levelcontainer.innerHTML += `
+          <div class="p-4 text-center text-bold w-[150px] rounded-lg shadow-md  ">
             <li id="${cat.id}" class="space-y-2 hover:bg-green-500">
                 ${cat.category_name}
             </li>
+          </div>
+
         `;
     });
 };
@@ -37,8 +62,26 @@ levelcontainer.addEventListener("click", (e) => {
        
         e.target.classList.add("bg-green-500");
 
-        console.log("Selected:", e.target.id);
+        //console.log("Selected:", e.target.id);
+        plantsByCategory(e.target.id);
     }
 });
 
+
+//plants by category
+const plantsByCategory = (categoryId) => {
+    console.log(categoryId);
+    fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.plants);
+            showPlantsCategories(data.plants);
+            })
+        .catch(error =>{ 
+            console.log(error)
+
+        });
+
+}
 loadcategory();
+plantsByCategory(1);
